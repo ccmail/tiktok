8e9aab86f62c48efb26ddadb2ced182ca21345d7
ccmail <m18639415833@163.com>
2023-02-22 04:40:11 +0800

    2023/2/22 4:39 更新mysql索引

--- model/comment.go
+++ model/comment.go
@@ -7,7 +7,7 @@ import (
 type Comment struct {
 	gorm.Model
 	UserID      uint
-	VideoID     uint
+	VideoID     uint `gorm:"index:vid"`
 	CommentText string
 	Valid       bool `db:"default:true"`
 }
--- model/message.go
+++ model/message.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Message struct {
 	//消息的ID
 	gorm.Model
-	UserID      uint
-	FriendID    uint
+	UserID      uint `gorm:"index:user_friend_union_idx"`
+	FriendID    uint `gorm:"index:user_friend_union_idx"`
 	MessageText string
 }
